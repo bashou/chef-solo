e3b0d50ae31fd787efa250282e0ba2962947ed44
Nassim Ait Boufrad <nassim.aitboufrad@francetv.fr>
2013-03-21 14:00:53 +0100

    Test install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,58 @@
 #!/bin/bash
 
-# This runs as root on the server
+# <udf name="hostname" label="Hostname">
+# <udf name="default_ruby_interpreter" label="Default Ruby Interpreter"
+#   oneOf="ruby-1.8.6,ruby-1.8.7,ruby-1.9.1,ruby-1.9.2,ruby-head" 
+#   default="ruby-1.9.2">
 
-chef_binary=/var/lib/gems/1.9.1/bin/chef-solo
+logfile=/tmp/chef.log
 
-# Are we on a vanilla system?
-if ! test -f "$chef_binary"; then
-    export DEBIAN_FRONTEND=noninteractive
-    # Upgrade headlessly (this is only safe-ish on vanilla systems)
-    aptitude update &&
-    apt-get -o Dpkg::Options::="--force-confnew" \
-        --force-yes -fuy dist-upgrade &&
-    # Install Ruby and Chef
-    aptitude install -y ruby1.9.1 ruby1.9.1-dev make &&
-    sudo gem1.9.1 install --no-rdoc --no-ri chef --version 0.10.0
-fi &&
+echo "Setting locales (fr_FR.UTF-8)" >> $logfile
+aptitude -y update
+aptitude -y install locales
+# Set locales to fr_FR.UTF-8
+localedev -i fr_FR -c -f UTF-8 fr_FR.UTF-8
+localgen
 
-"$chef_binary" -c solo.rb -j solo.json
+echo "Upgrading packages" >> $logfile
+aptitude -y upgrade
+
+#echo "Setting hostname to {$HOSTNAME}" >> $logfile
+#echo ${HOSTNAME} > /etc/hostname
+#hostname -F /etc/hostname
+
+echo "Installing RVM and Ruby dependencies" >> $logfile
+apt-get -y install curl git-core bzip2 build-essential zlib1g-dev libssl-dev
+
+echo "Installing RVM system-wide" >> $logfile
+bash -c "bash <( curl -L https://raw.github.com/wayneeseguin/rvm/stable/binscripts/rvm-installer ) --version 'stable'"
+cat >> /etc/profile <<'EOF'
+# Load RVM if it is installed,
+#  first try to load  user install
+#  then try to load root install, if user install is not there.
+if [ -s "$HOME/.rvm/scripts/rvm" ] ; then
+  . "$HOME/.rvm/scripts/rvm"
+elif [ -s "/usr/local/rvm/scripts/rvm" ] ; then
+  . "/usr/local/rvm/scripts/rvm"
+fi
+EOF
+
+source /etc/profile
+
+echo "Installing Ruby interpreter (${DEFAULT_RUBY_INTERPRETER})" >> $logfile
+rvm install ${DEFAULT_RUBY_INTERPRETER}
+rvm use ${DEFAULT_RUBY_INTERPRETER} --default
+
+echo "Installing Chef" >> $logfile
+gem install chef
+
+echo "Configuring Chef solo" >> $logfile
+mkdir /etc/chef
+cat >> /etc/chef/solo.rb <<EOF
+file_cache_path "/tmp/chef"
+cookbook_path "/tmp/chef/cookbooks"
+role_path "/tmp/chef/roles"
+log_level :info
+EOF
+
+echo "Script complete" >> $logfile
